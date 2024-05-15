6538f35e8f5ab1e2fcf8fdd70e616573e3e89a29
Dexter Crago <jodieanddexter@gmail.com>
2024-05-15 15:05:13 -0600

    Allow author to delete comments on published posts

--- frontend-owner/modules/createCommentsSection.js
+++ frontend-owner/modules/createCommentsSection.js
@@ -1,4 +1,5 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import fullPostController from './fullPostController.js';
 import getComments from './getComments.js';
 
 export default async function createCommentsSection(post) {
@@ -25,6 +26,29 @@ export default async function createCommentsSection(post) {
     const body = document.createElement('p');
     body.textContent = comment.body;
     commentElement.appendChild(body);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete Comment';
+    deleteBtn.addEventListener('click', () => {
+      const deleteConfirmed = prompt(
+        'This action cannot be undone. To delete this comment, type "Delete" and hit enter.'
+      );
+
+      if (deleteConfirmed === 'Delete') {
+        fetch(
+          `http://localhost:3000/posts/${post._id}/delete-comment/${comment._id}`,
+          {
+            method: 'POST',
+          }
+        )
+          .then((response) => response.json())
+          .then((json) => {
+            fullPostController(post);
+            console.log(json);
+          });
+      }
+    });
+    commentElement.appendChild(deleteBtn);
 
     commentsSection.appendChild(commentElement);
   });
