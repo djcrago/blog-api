e18fd4efd3cd022ff5af01a5d12d790f8049df71
Dexter Crago <jodieanddexter@gmail.com>
2024-05-15 09:18:16 -0600

    Remove unnecessary module

--- frontend-owner/drafts.js
+++ frontend-owner/drafts.js
@@ -1,3 +1,18 @@
-import renderDraftPosts from './modules/renderDraftPosts.js';
+import fetchPosts from './fetchPosts.js';
+import createPostPreview from './createPostPreview.js';
+import renderPostPreviews from './renderPostPreviews.js';
 
-renderDraftPosts();
+export default async function renderDraftPosts() {
+  const arrayOfPosts = await fetchPosts();
+
+  const postPreviews = [];
+
+  arrayOfPosts.forEach((post) => {
+    if (!post.published) {
+      const postElement = createPostPreview(post);
+      postPreviews.push(postElement);
+    }
+  });
+
+  renderPostPreviews(postPreviews);
+}
