c50d162f7257e85793043bde0940589c90d40d03
Dexter Crago <jodieanddexter@gmail.com>
2024-05-15 10:46:34 -0600

    Clean up/rename modules that create html elements

--- frontend-owner/modules/createFullPost.js
+++ frontend-owner/modules/createFullPost.js
@@ -1,22 +1,22 @@
 import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
 export default function createFullPost(post) {
-  const postContainer = document.createElement('div');
-  postContainer.classList.toggle('full-post');
+  const fullPost = document.createElement('div');
+  fullPost.classList.toggle('full-post');
 
   const title = document.createElement('h2');
   title.textContent = post.title;
-  postContainer.appendChild(title);
+  fullPost.appendChild(title);
 
   const info = document.createElement('p');
   const author = `${post.author.first_name} ${post.author.last_name}`;
   const date = DateTime.fromISO(post.date).toLocaleString(DateTime.DATE_MED);
   info.textContent = `${author} - ${date}`;
-  postContainer.appendChild(info);
+  fullPost.appendChild(info);
 
   const body = document.createElement('p');
   body.textContent = post.body;
-  postContainer.appendChild(body);
+  fullPost.appendChild(body);
 
-  return postContainer;
+  return fullPost;
 }
