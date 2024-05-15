e4d31059faf59ba6403c2e74cb3edbbbeb244d7f
Dexter Crago <jodieanddexter@gmail.com>
2024-05-15 10:48:20 -0600

    Remove/rename modules
    The new names show more consistency throughout the app. Also, the
    previous overabundance of modules was confusing and hard to follow.
    By using the same modules, but passing in a boolean to indicate whether
    a published post or draft is needed, repition is reduced and things are
    easier to follow overall.

--- frontend-owner/modules/fetchPostComments.js
+++
@@ -1,9 +0,0 @@
-export default async function fetchPostComments(post) {
-  const rawResponse = await fetch(
-    `http://127.0.0.1:3000/posts/${post._id}/comments`
-  );
-
-  const arrayOfPostComments = await rawResponse.json();
-
-  return arrayOfPostComments;
-}
