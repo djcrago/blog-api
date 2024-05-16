a71c986030734979d1494f02563ffa4f0204c49f
Dexter Crago <jodieanddexter@gmail.com>
2024-05-16 11:28:20 -0600

    Add signup page to allow authors to create an account

---
+++ frontend-owner/signUp.js
@@ -0,0 +1,34 @@
+const signUpForm = document.querySelector('#sign-up-form');
+const firstName = document.querySelector('#first_name');
+const lastName = document.querySelector('#last_name');
+const usernameField = document.querySelector('#username');
+const passwordField = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password_confirm');
+const authorPasscode = document.querySelector('#author_passcode');
+
+signUpForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  fetch('http://localhost:3000/users/sign-up', {
+    method: 'POST',
+    body: JSON.stringify({
+      first_name: firstName.value,
+      last_name: lastName.value,
+      username: usernameField.value,
+      password: passwordField.value,
+      password_confirm: passwordConfirm.value,
+      author_passcode: authorPasscode.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      if (json.message === 'user created successfully') {
+        window.location.href = 'index.html';
+      }
+      // Else display error messages and do not redirect
+    });
+});
