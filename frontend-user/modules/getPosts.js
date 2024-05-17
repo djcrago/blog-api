22a6dec967e152346b8e3bb86d4ddf7601b87cda
Dexter Crago <jodieanddexter@gmail.com>
2024-05-17 14:23:24 -0600

    Update frontend-user app to fetch from /posts/public

--- frontend-user/modules/getPosts.js
+++ frontend-user/modules/getPosts.js
@@ -1,5 +1,5 @@
 export default async function getPosts() {
-  const rawResponse = await fetch('http://127.0.0.1:3000/posts');
+  const rawResponse = await fetch('http://127.0.0.1:3000/posts/public');
 
   const arrayOfPosts = await rawResponse.json();
 
