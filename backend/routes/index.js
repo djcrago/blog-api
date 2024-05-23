7de42def0fb70b7b84dcbbe0c42df5e02f1533aa
Dexter Crago <jodieanddexter@gmail.com>
2024-05-22 18:19:45 -0600

    Route to GitHub page on '/' route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.redirect('/posts');
+  res.redirect('https://github.com/djcrago/blog-api');
 });
 
 module.exports = router;
